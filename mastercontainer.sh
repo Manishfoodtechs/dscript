9765cb782fca8abb643e7f03ffdc71d748d14f3b
manish srivastava <manishfoodtechs@gmail.com>
2023-02-05 22:46:54 +0530

    Update mastercontainer.sh

--- mastercontainer.sh
+++ mastercontainer.sh
@@ -5,8 +5,8 @@ docker pull ubuntu
 
 # Set environment variables
 MYSQL_PASSWORD=secret
-FQDN=example.com
-EMAIL=emailmanish84@gmail.com
+FQDN=cbeindia.com
+EMAIL=emailmanish84.gmail.com
 
 # Create the Docker container
 docker run -d --name nginx-mysql-modsecurity-letsencrypt-phpmyadmin \
@@ -16,35 +16,16 @@ docker run -d --name nginx-mysql-modsecurity-letsencrypt-phpmyadmin \
   ubuntu bash -c '
     # Update the package repository and install Nginx, MySQL, ModSecurity, Let's Encrypt SSL certificates, and phpMyAdmin
     apt-get update && \
-    apt-get install -y nginx mysql-server libapache2-mod-security2 certbot python3-certbot-nginx phpmyadmin
-    
-    # Create the Nginx default configuration file if it doesn't exist
-    if [ ! -f /etc/nginx/conf.d/default.conf ]; then
-      touch /etc/nginx/conf.d/default.conf
-    fi
-    
-    # Modify the Nginx default configuration file
+    apt-get install -y nginx mysql-server libnginx-mod-http-modsecurity certbot python3-certbot-nginx phpmyadmin && \
     sed -i "s/.*listen.*/listen 80;/" /etc/nginx/conf.d/default.conf && \
     sed -i "s/.*server_name.*/server_name $FQDN;/" /etc/nginx/conf.d/default.conf && \
+    certbot --nginx -d $FQDN --email $EMAIL
     
-    # Obtain a Let's Encrypt SSL certificate for the specified domain
-certbot --nginx -d $FQDN --email $EMAIL
-   
     # Configure phpMyAdmin
-rm -rf /var/www/html/phpmyadmin && \
-ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin && \
-sed -i "s/.*listen.*/listen 80;/" /etc/phpmyadmin/apache.conf && \
-sed -i "s/.*server_name.*/server_name $FQDN;/" /etc/phpmyadmin/apache.conf
-
-    
-    # Create the phpMyAdmin Apache configuration file if it doesn't exist
-    if [ ! -f /etc/phpmyadmin/apache.conf ]; then
-      touch /etc/phpmyadmin/apache.conf
-    fi
-    
-    # Modify the phpMyAdmin Apache configuration file
-    sed -i "s/.*listen.*/listen 80;/" /etc/phpmyadmin/apache.conf && \
-    sed -i "s/.*server_name.*/server_name $FQDN;/" /etc/phpmyadmin/apache.conf
+    ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin && \
+    sed -i "s/.*listen.*/listen 80;/" /etc/nginx/sites-available/phpmyadmin.conf && \
+    sed -i "s/.*server_name.*/server_name $FQDN;/" /etc/nginx/sites-available/phpmyadmin.conf && \
+    ln -s /etc/nginx/sites-available/phpmyadmin.conf /etc/nginx/sites-enabled/
     
     # Start Nginx and MySQL
     service nginx start && \
