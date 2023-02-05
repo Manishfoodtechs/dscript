beb40e88b329488f8219eecf8c1ece3340327830
manish srivastava <manishfoodtechs@gmail.com>
2023-02-05 22:51:46 +0530

    Update mastercontainer.sh

--- mastercontainer.sh
+++ mastercontainer.sh
@@ -5,8 +5,7 @@ docker pull ubuntu
 
 # Set environment variables
 MYSQL_PASSWORD=secret
-FQDN=cbeindia.com
-EMAIL=emailmanish84.gmail.com
+FQDN=example.com
 
 # Create the Docker container
 docker run -d --name nginx-mysql-modsecurity-letsencrypt-phpmyadmin \
@@ -16,16 +15,11 @@ docker run -d --name nginx-mysql-modsecurity-letsencrypt-phpmyadmin \
   ubuntu bash -c '
     # Update the package repository and install Nginx, MySQL, ModSecurity, Let's Encrypt SSL certificates, and phpMyAdmin
     apt-get update && \
-    apt-get install -y nginx mysql-server libnginx-mod-http-modsecurity certbot python3-certbot-nginx phpmyadmin && \
+    apt-get install -y nginx mysql-server certbot phpmyadmin && \
     sed -i "s/.*listen.*/listen 80;/" /etc/nginx/conf.d/default.conf && \
     sed -i "s/.*server_name.*/server_name $FQDN;/" /etc/nginx/conf.d/default.conf && \
-    certbot --nginx -d $FQDN --email $EMAIL
-    
-    # Configure phpMyAdmin
-    ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin && \
-    sed -i "s/.*listen.*/listen 80;/" /etc/nginx/sites-available/phpmyadmin.conf && \
-    sed -i "s/.*server_name.*/server_name $FQDN;/" /etc/nginx/sites-available/phpmyadmin.conf && \
-    ln -s /etc/nginx/sites-available/phpmyadmin.conf /etc/nginx/sites-enabled/
+    certbot --nginx -d $FQDN && \
+    ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin
     
     # Start Nginx and MySQL
     service nginx start && \
