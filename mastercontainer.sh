e9242f1da9f82eba11778eba664949f7b017743c
manish srivastava <manishfoodtechs@gmail.com>
2023-02-05 23:33:55 +0530

    Update mastercontainer.sh

--- mastercontainer.sh
+++ mastercontainer.sh
@@ -8,17 +8,16 @@ MYSQL_PASSWORD=secret
 FQDN=example.com
 
 # Create the Docker container
-docker run -d --name nginx-mysql-modsecurity-letsencrypt-phpmyadmin \
+docker run -d --name master \
   -e MYSQL_PASSWORD=$MYSQL_PASSWORD \
-  -e FQDN=$FQDN \
   -p 80:80 \
--p 443:443 \
--p 3306:3306 \
+  -p 3306:3306 \
+  -e FQDN=$FQDN \
   ubuntu bash -c '
     # Update the package repository and install Nginx, MySQL, ModSecurity, Let's Encrypt SSL certificates, and phpMyAdmin
     apt-get update && \
     apt-get install -y nginx mysql-server certbot phpmyadmin && \
-    sed -i "s/.*listen.*/listen 3306;/" /etc/nginx/conf.d/default.conf && \
+    sed -i "s/.*listen.*/listen 80;/" /etc/nginx/conf.d/default.conf && \
     sed -i "s/.*server_name.*/server_name $FQDN;/" /etc/nginx/conf.d/default.conf && \
     certbot --nginx -d $FQDN && \
     ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin
