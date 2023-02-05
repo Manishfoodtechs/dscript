f5b0207d874754b27e64b9daebe517126e8a5aea
manish srivastava <manishfoodtechs@gmail.com>
2023-02-05 22:12:42 +0530

    Update mastercontainer.sh

--- mastercontainer.sh
+++ mastercontainer.sh
@@ -15,13 +15,27 @@ docker run -d --name nginx-mysql-modsecurity-letsencrypt-phpmyadmin \
   ubuntu bash -c '
     # Update the package repository and install Nginx, MySQL, ModSecurity, Let's Encrypt SSL certificates, and phpMyAdmin
     apt-get update && \
-    apt-get install -y nginx mysql-server libapache2-mod-security2 certbot python3-certbot-nginx phpmyadmin && \
+    apt-get install -y nginx mysql-server libapache2-mod-security2 certbot python3-certbot-nginx phpmyadmin
+    
+    # Create the Nginx default configuration file if it doesn't exist
+    if [ ! -f /etc/nginx/conf.d/default.conf ]; then
+      touch /etc/nginx/conf.d/default.conf
+    fi
+    
+    # Modify the Nginx default configuration file
     sed -i "s/.*listen.*/listen 80;/" /etc/nginx/conf.d/default.conf && \
     sed -i "s/.*server_name.*/server_name $FQDN;/" /etc/nginx/conf.d/default.conf && \
     certbot --nginx -d $FQDN
     
     # Configure phpMyAdmin
-    ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin && \
+    ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin
+    
+    # Create the phpMyAdmin Apache configuration file if it doesn't exist
+    if [ ! -f /etc/phpmyadmin/apache.conf ]; then
+      touch /etc/phpmyadmin/apache.conf
+    fi
+    
+    # Modify the phpMyAdmin Apache configuration file
     sed -i "s/.*listen.*/listen 80;/" /etc/phpmyadmin/apache.conf && \
     sed -i "s/.*server_name.*/server_name $FQDN;/" /etc/phpmyadmin/apache.conf
     
