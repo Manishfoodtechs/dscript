d2b423445c37ff2abedbd5bb973a70914fc41876
manish srivastava <manishfoodtechs@gmail.com>
2023-02-05 23:18:01 +0530

    Update mastercontainer.sh

--- mastercontainer.sh
+++ mastercontainer.sh
@@ -12,11 +12,13 @@ docker run -d --name nginx-mysql-modsecurity-letsencrypt-phpmyadmin \
   -e MYSQL_PASSWORD=$MYSQL_PASSWORD \
   -e FQDN=$FQDN \
   -p 80:80 \
+-p 443:443 \
+-p 3306:3306 \
   ubuntu bash -c '
     # Update the package repository and install Nginx, MySQL, ModSecurity, Let's Encrypt SSL certificates, and phpMyAdmin
     apt-get update && \
     apt-get install -y nginx mysql-server certbot phpmyadmin && \
-    sed -i "s/.*listen.*/listen 80;/" /etc/nginx/conf.d/default.conf && \
+    sed -i "s/.*listen.*/listen 3306;/" /etc/nginx/conf.d/default.conf && \
     sed -i "s/.*server_name.*/server_name $FQDN;/" /etc/nginx/conf.d/default.conf && \
     certbot --nginx -d $FQDN && \
     ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin
